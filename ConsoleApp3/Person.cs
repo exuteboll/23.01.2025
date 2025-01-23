1eb2c9844446281c3cf4b081ed842609c4cb2f70
exuteboll <exuteboll4@gmail.com>
2025-01-23 18:34:09 +0200

    Add files via upload

---
+++ ConsoleApp3/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp3
+{
+    // Задание 1. Создать класс Person с полями: имя, фамилия, возраст и методом вывода всех данных на консоль.
+   
+    internal class Person
+    {
+         // Задание 1. Создать класс Person с полями: имя, фамилия, возраст и методом вывода всех данных на консоль.
+        public string FirstName = "Daniil"; //Поле имя
+
+        public string SecondName = "Demodov"; //Поле фамилия
+
+        public int age = 17; // поле возраста 
+        public void Print()
+        {
+            Console.WriteLine($"Имя: {FirstName}; фамилия: {SecondName}; Возраст: {age}");
+        }
+
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        // Задание 1. Создать класс Person с полями: имя, фамилия, возраст и методом вывода всех данных на консоль.
+        
+       
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            string FirstName = person.FirstName;
+            string SecondName = person.SecondName;
+            int Age = person.age;
+            Console.WriteLine($"Имя: {FirstName}\t Фамилия: {SecondName}\t Возраст: {Age}");
+            person.FirstName = "Егор";
+            person.SecondName = "Присич";
+            person.age = 17;
+            person.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
